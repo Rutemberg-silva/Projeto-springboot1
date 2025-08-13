1cb478b6fd238ec8ceef4be413f451424d9e4e19
Rutemberg-silva <rutemberg.silva@hotmail.com>
2025-08-13 14:04:11 -0300

    User entity and resource

---
+++ src/main/java/com/rutemberg/projeto1/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.rutemberg.projeto1.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rutemberg.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "berg", "berg.com", "999999", "123");
+		return ResponseEntity.ok().body(u);
+	}
+	
+
+}
